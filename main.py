630e7284032cd7911491951f97b8ef563a17c022
unknown <vasenevvladviktorovish@gmail.com>
2026-08-27 01:54:47 +0300

    add_DB

--- main.py
+++ main.py
@@ -1,23 +1,14 @@
 from uuid import uuid4
-
-from fastapi import FastAPI, status, HTTPException
+from contextlib import asynccontextmanager
+from fastapi import FastAPI, HTTPException, status, Depends
+from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy import create_engine, select
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, sessionmaker, Session
 from pydantic import BaseModel
 
-
-from fastapi.middleware.cors import CORSMiddleware
-
-app = FastAPI()
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-    ],
-    allow_methods=["*"],
-    allow_headers=["*"],
-    allow_credentials=True,
-)
-
+DATABASE_URL = "postgresql+psycopg://postgres:admin@127.0.0.1:15432/postgres"
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine)
 
 
 class Task(BaseModel):
@@ -33,47 +24,86 @@ class TaskUpdate(BaseModel):
     title: str | None = None
     completed: bool | None = None
 
-tasks: list[Task] = []
+
+
+
+class Base(DeclarativeBase):
+    id: Mapped[str] = mapped_column(primary_key=True, default=lambda: str(uuid4()))
+
+class TaskORM(Base):
+    __tablename__ = "tasks"
+    title: Mapped[str]
+    completed: Mapped[bool] = mapped_column(default=False)
+
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    Base.metadata.create_all(bind=engine)
+    yield
+
+app = FastAPI(lifespan=lifespan)  # ← ОДИН РАЗ
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True,
+)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+
+def task_to_model(task: TaskORM) -> Task:
+    """Конвертация объекта ORM в Pydantic"""
+    return Task(id=task.id, title=task.title, completed=task.completed)
 
 
 @app.get("/tasks", response_model=list[Task])
-def get_tasks():
+def get_tasks(db: Session = Depends(get_db)) -> list[Task]:
     """Получить список задач"""
-    return tasks
+    tasks = db.scalars(select(TaskORM)).all()
+    return [task_to_model(task) for task in tasks]
 
 
 @app.post("/tasks", response_model=Task, status_code=status.HTTP_201_CREATED)
-def create_task(payload: TaskCreate):
+def create_task(payload: TaskCreate, db: Session = Depends(get_db)) -> Task:
     """Создать новую задачу"""
-    task = Task(id=str(uuid4()), title=payload.title, completed=False)
-    tasks.append(task)
-    return task
+    task = TaskORM(title=payload.title, completed=False)
+
+    db.add(task)
+    db.commit()
+    return task_to_model(task)
 
 
-@app.patch("/tasks/{task_id}", response_model=Task, status_code=status.HTTP_200_OK)
-def update_task(task_id: str, payload: TaskUpdate):
-    """Обонвить новую задачу"""
-    for task in tasks:
-        if task.id == task_id:
-            if payload.title is not None:
-                task.title = payload.title
-            if payload.completed is not None:
-                task.completed = payload.completed
-    
-            return task
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Задача не найдена")
+@app.patch("/tasks/{task_id}", response_model=Task)
+def update_task(task_id: str, payload: TaskUpdate, db: Session = Depends(get_db)) -> Task:
+    """
+    Обновить существующую задачу
+    task_id получаем из url
+    payload получаем из тела запроса
+    """
+    task = db.get(TaskORM, task_id)
+    if task is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Задача не найдена")
+
+    task.title = payload.title if payload.title is not None else task.title
+    task.completed = payload.completed if payload.completed is not None else task.completed
+    db.commit()
+    return task_to_model(task)
 
 
 @app.delete("/tasks/{task_id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_task(task_id: str):
+def delete_task(task_id: str, db: Session = Depends(get_db)) -> None:
     """Удалить задачу"""
-    for task in tasks:
-        if task.id == task_id:
-           tasks.remove(task)
-           return
-    
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Задача не найдена")
+    task = db.get(TaskORM, task_id)
+    if task is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Задача не найдена")
 
-
-
-
+    db.delete(task)
+    db.commit()
